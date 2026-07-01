bae2ff89a3cd49e6c10cdc626e4d110d11e257ac
Anubhav Khandelwal <50150129+anubhav2707@users.noreply.github.com>
2026-07-02 01:20:54 +0530

    feat: add isValidIndianPincode validator with comprehensive tests

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,2 @@
 // Export all validators
-export { isValidEmail, isValidPhone, isValidIndianPincode } from './validators';
-
-// Re-export for convenience
-export * from './validators';
+export { isValidIndianPincode } from './validators';
